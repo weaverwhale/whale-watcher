1930c84b5bf31c44ba1a5f8cf96ef397c6a177e3
Mike Weaver <michael@triplewhale.com>
2023-10-10 12:07:46 -0400

    Add more integrations; update demo data

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,18 +1,29 @@
 import { useState, useEffect } from 'react'
 import Marquee from 'react-fast-marquee'
 import TestData from './data'
+
 import Whale from '../assets/whale.svg'
 import Blended from '../assets/blended.svg'
-import GA from '../assets/ga.svg'
-import FB from '../assets/fb.svg'
+import FACEBOOK from '../assets/facebook.svg'
+import GOOGLE from '../assets/google.svg'
+import TIKTOK from '../assets/tiktok.svg'
+import TWITTER from '../assets/twitter.svg'
+import PINTEREST from '../assets/pinterest.svg'
+import SNAPCHAT from '../assets/snapchat.svg'
 
 import './Popup.css'
+
+const isDev = import.meta.env.DEV
 
 function formatKey(key: string) {
   return key
     .replace('_', ' ')
     .replace('fb', '')
     .replace('ga', '')
+    .replace('tt', '')
+    .replace('tw', '')
+    .replace('pin', '')
+    .replace('sc', '')
     .replace('roas', 'ROAS')
     .replace('cpm', 'CPM')
     .replace('ctr', 'CTR')
@@ -22,9 +33,17 @@ function formatKey(key: string) {
 
 function formatImage(key: string) {
   if (key.includes('fb')) {
-    return FB
+    return FACEBOOK
   } else if (key.includes('ga')) {
-    return GA
+    return GOOGLE
+  } else if (key.includes('tt')) {
+    return TIKTOK
+  } else if (key.includes('tw')) {
+    return TWITTER
+  } else if (key.includes('pin')) {
+    return PINTEREST
+  } else if (key.includes('sc')) {
+    return SNAPCHAT
   } else if (key.includes('blended')) {
     return Blended
   } else {
@@ -35,12 +54,15 @@ function formatImage(key: string) {
 function App() {
   const [data, setData] = useState<any>({})
   const [isPopout, setIsPopout] = useState<boolean>(!!window.location.search.includes('popout'))
+  const [speed, setSpeed] = useState<number>(50)
 
   useEffect(() => {
-    if (import.meta.env.DEV) {
+    if (isDev) {
       setData(TestData)
     } else {
-      fetch('https://staging.api.triplewhale.com/api/v2/bi/get-benchmarks-for-marketing')
+      fetch(
+        'https://staging.api.triplewhale.com/api/v2/bi/get-benchmarks-for-marketing?requester=triplewhale',
+      )
         .then((res) => res.json())
         .then((res) => setData(res))
     }
@@ -75,7 +97,7 @@ function App() {
         )}
       </div>
       {data && data.total && (
-        <Marquee pauseOnHover={true}>
+        <Marquee pauseOnHover={true} speed={speed}>
           {Object.keys(data.total).map((key: string) => (
             <p className="tw-ticker" key={key}>
               <img src={formatImage(key)} /> {formatKey(key)} •{' '}
